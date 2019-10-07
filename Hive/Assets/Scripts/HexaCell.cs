45ce62bd500a899eb8423132cffe7db2c3553da0
talioo1997 <talioo1997@gmail.com>
2019-10-07 15:13:17 +0300

    Add project

---
+++ Hive/Assets/Scripts/HexaCell.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class HexaCell : MonoBehaviour
+{
+    [SerializeField] private SOHexaInfo hexaInfo;
+    [SerializeField] private CellParams cellParams;
+
+    private void Start()
+    {
+        if (!cellParams.IsEmpty) hexaInfo.AddNewCell(this);
+    }
+    private void OnMouseDown()
+    {
+        if(cellParams.IsEmpty)
+        {
+            CreateNewCell();
+        }
+    }
+    private void CreateNewCell()
+    {
+        Instantiate(hexaInfo.hexaPrefab, transform.position, transform.rotation);
+        cellParams.IsEmpty = false;
+        hexaInfo.AddNewCell(this);
+    }
+}
+[System.Serializable]
+public class CellParams
+{
+    public bool IsEmpty = true;
+}
---
+++ Hive/Assets/Scripts/SOHexaInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Hexa Info", menuName = "Custom/HexaInfo")]
+public class SOHexaInfo : ScriptableObject
+{
+    public GameObject hexaPrefab;
+
+    [SerializeField] private List<HexaCell> cellsOnScene;
+
+    public void AddNewCell(HexaCell cell)
+    {
+        cellsOnScene.Add(cell);
+    }
+}
